41755624f517663898d90f1c8a85c4819a446f0b
David Álvarez de la Torre <davidalvarezdlt@gmail.com>
2020-05-26 11:15:08 +0200

    things

--- samplernn_pase/utils.py
+++ samplernn_pase/utils.py
@@ -1,7 +1,4 @@
 import re
-import ahoproc_tools
-import numpy as np
-from concurrent.futures import ThreadPoolExecutor
 
 lab_regex = r'([0-9]+) ([0-9]+) '
 lab_regex += r'(.+)\^(.+)-(.+)\+(.+)=(.+)@(.+)_(.+)'
@@ -43,22 +40,12 @@ class SampleRNNQuantizer:
         return self.q_levels // 2
 
     def quantize(self, samples):
-        if self.q_type == self.LINEAR_QUANT:
-            return self.linear_quantize(samples=samples)
-        elif self.q_type == self.ULAW_QUANT:
-            return self.ulaw_quantize(samples=samples)
-        else:
-            raise NotImplemented('Unrecognized q_type')
+        return self.quantize_linear(samples) if self.q_type == self.LINEAR_QUANT else self.quantize_ulaw(samples)
 
     def dequantize(self, samples):
-        if self.q_type == self.LINEAR_QUANT:
-            return self.linear_dequantize(samples=samples)
-        elif self.q_type == self.ULAW_QUANT:
-            return self.ulaw_dequantize(samples=samples)
-        else:
-            raise NotImplemented('Unrecognized q_type')
+        return self.dequantize_linear(samples) if self.q_type == self.LINEAR_QUANT else self.dequantize_ulaw(samples)
 
-    def linear_quantize(self, samples):
+    def quantize_linear(self, samples):
         samples = samples.clone()
         samples -= samples.min(dim=-1)[0].expand_as(samples)
         samples /= samples.max(dim=-1)[0].expand_as(samples)
@@ -66,100 +53,21 @@ class SampleRNNQuantizer:
         samples += self._EPSILON / 2
         return samples.long()
 
-    def linear_dequantize(self, samples):
+    def dequantize_linear(self, samples):
         return samples.float() / (self.q_levels / 2) - 1
 
-    def ulaw_quantize(self, samples):
-        return self.midrise(self.ulaw(samples))
+    def quantize_ulaw(self, x, max_value=1.0):
+        # Convert to uLaw
+        x = x.sign() * ((self._MU / max_value) * x.abs() + 1.).log() / self._LOG_MU1
+        # Midrise encoding
+        y = 0.5 * (x + 1.0)
+        y *= (self.q_levels - self._EPSILONs)
+        return y.long()
 
-    def ulaw_dequantize(self, samples):
-        return self.iulaw(self.imidrise(samples))
-
-    def ulaw(self, x, max_value=1.0):
-        v = self._MU / max_value
-        y = x.sign() * (v * x.abs() + 1.).log() / self._LOG_MU1
-        return y
-
-    def iulaw(self, c):
-        x = (c.abs() * self._LOG_MU1).exp() - 1
-        y = c.sign() * x / self._MU
-        return y
-
-    def midrise(self, x):
-        x = 0.5 * (x + 1.0)
-        x *= (self.q_levels - self._EPSILONs)
-        return x.long()
-
-    def imidrise(self, xq):
-        return xq.float() * 2.0 / self.q_levels - 1.0
-
-
-class SampleRNNAhocoder:
-
-    @staticmethod
-    def read_acoustic_conds(utterance_path: str):
-        # Load the Conds Files
-        generated_utterance_cc = ahoproc_tools.io.read_aco_file(utterance_path + '.cc', (-1, 40))
-        generated_utterance_fv = ahoproc_tools.io.read_aco_file(utterance_path + '.fv', (-1,))
-        generated_utterance_lf0 = ahoproc_tools.io.read_aco_file(utterance_path + '.lf0', (-1,))
-
-        # Interpolate FV and LF0, obtain VU
-        generated_utterance_fv, _ = ahoproc_tools.interpolate.interpolation(generated_utterance_fv, 1e3)
-        generated_utterance_lf0, generated_utterance_vu = ahoproc_tools.interpolate.interpolation(
-            generated_utterance_lf0, -1e10)
-
-        # Unsqueeze dimensions
-        generated_utterance_fv = np.expand_dims(generated_utterance_fv, axis=1)
-        generated_utterance_lf0 = np.expand_dims(generated_utterance_lf0, axis=1)
-        generated_utterance_vu = np.expand_dims(generated_utterance_vu, axis=1)
-
-        # Return 43 conditionants
-        return np.concatenate(
-            (generated_utterance_cc, generated_utterance_fv, generated_utterance_lf0, generated_utterance_vu),
-            axis=1
-        )
-
-    @staticmethod
-    def generate_acoustic_conds(utterances_paths):
-        with ThreadPoolExecutor(max_workers=50) as executor:
-            for utterance_path in utterances_paths:
-                executor.submit(ahoproc_tools.io.wav2aco, utterance_path)
-
-    @staticmethod
-    def do_compute_objective_metrics(original_parameters, generated_parameters):
-        cc_mcd = ahoproc_tools.error_metrics.MCD(original_parameters[:, :40], generated_parameters[:, :40])
-        lf0_rmse = ahoproc_tools.error_metrics.RMSE(np.exp(original_parameters[:, 41]),
-                                                    np.exp(generated_parameters[:, 41]))
-        vu_afpr = ahoproc_tools.error_metrics.AFPR(original_parameters[:, 42], generated_parameters[:, 42])
-        return cc_mcd, lf0_rmse, vu_afpr
-
-    @staticmethod
-    def compute_objective_metrics(data_info, experiment, epoch_n, pase_seed_duration=None):
-        # Create lists to store error metrics
-        lf0_rmse = []
-        vu_afpr = []
-        cc_mcd = []
-
-        # Iterate over all the utterances
-        for data_info_item in data_info:
-            # Get Paths of original and generated conds
-            utterance_conds_path = data_info_item['dataset']['conds_utterance']['acoustic_folder_path'] + \
-                                   data_info_item['utterance']['path']
-            utterance_generated_conds_path = \
-                experiment.generated_samples_path + experiment.get_generated_sample_name(data_info_item, epoch_n,
-                                                                                         pase_seed_duration)
-
-            # Get Acoustic conds from both files
-            utterance_acoustic_conds = SampleRNNAhocoder.read_acoustic_conds(utterance_conds_path)
-            utterance_generated_acoustic_conds = SampleRNNAhocoder.read_acoustic_conds(utterance_generated_conds_path)
-
-            # Compute Measures
-            cc_mcd.append(ahoproc_tools.error_metrics.MCD(
-                utterance_acoustic_conds[:, :40], utterance_generated_acoustic_conds[:, :40]))
-            lf0_rmse.append(ahoproc_tools.error_metrics.RMSE(
-                np.exp(utterance_acoustic_conds[:, 41]), np.exp(utterance_generated_acoustic_conds[:, 41])))
-            vu_afpr.append(ahoproc_tools.error_metrics.AFPR(
-                utterance_acoustic_conds[:, 42], utterance_generated_acoustic_conds[:, 42]))
-
-        # Return MEAN error metrics
-        return np.mean(cc_mcd), np.mean(lf0_rmse), np.mean(vu_afpr, axis=0)
+    def dequantize_ulaw(self, y):
+        # Midrise decoding
+        y = y.float() * 2.0 / self.q_levels - 1.0
+        # Inverse uLaw
+        x = (y.abs() * self._LOG_MU1).exp() - 1
+        x = x.sign() * x / self._MU
+        return x
